dd6aa748a78bbc4f5be3715b4795aef620cd15cd
Chris Lane <chris.lane359@gmail.com>
2017-12-29 08:08:17 -0800

    Importing routing stuff

--- react-a11y-demo/src/App.js
+++ react-a11y-demo/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Helmet } from "react-helmet";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
