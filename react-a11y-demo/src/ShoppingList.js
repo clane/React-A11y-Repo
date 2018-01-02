12fe082077a296853fd60e576f061cc4f304915c
Chris Lane <chris.lane359@gmail.com>
2018-01-01 20:57:57 -0800

    now setting titles

--- react-a11y-demo/src/ShoppingList.js
+++ react-a11y-demo/src/ShoppingList.js
@@ -5,6 +5,11 @@ class ShoppingList extends React.Component {
   render() {
     return (
       <div className="shopping-list">
+        <Helmet>
+          <html lang="en" />
+          <meta charSet="utf-8" />
+          <title>Shopping List</title>
+        </Helmet>
         <h1>Shopping List for {this.props.name}</h1>
         <ul>
           <li>Instagram</li>
