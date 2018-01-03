b8d2a9af965028e96b6e246ac2975e06180c13b2
Chris Lane <chris.lane359@gmail.com>
2018-01-02 17:10:26 -0800

    fixed scoring to support square numbering 1-9

--- react-a11y-demo/src/Game.js
+++ react-a11y-demo/src/Game.js
@@ -185,16 +185,18 @@ export class Game extends Component {
 
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
+
+const lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
     [1, 4, 7],
     [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [1, 5, 9],
+    [3, 5, 7]
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
