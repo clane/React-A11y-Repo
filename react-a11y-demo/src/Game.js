a14fc184f6a943098471867439d68d146604fb33
Chris Lane <chris.lane359@gmail.com>
2018-01-05 07:44:19 -0800

    trying a live region for status

--- react-a11y-demo/src/Game.js
+++ react-a11y-demo/src/Game.js
@@ -51,7 +51,7 @@ class Board extends Component {
       xIsNext: !this.state.xIsNext,
       activeD: this.state.activeD
     });
-    this.statusContainer.focus();
+    //this.statusContainer.focus();
   }
 
   handleKeyboard(e) {
@@ -156,6 +156,7 @@ class Board extends Component {
             ref={status => {
               this.statusContainer = status;
             }}
+            aria-live="assertive"
           >
             {status}
           </div>
