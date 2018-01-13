b5bfd5b69bb935ef0579eea6fbc208a6d441e1d4
Chris Lane <chris.lane359@gmail.com>
2018-01-13 12:16:04 -0800

    added content to home

--- react-a11y-demo/src/HomePage.js
+++ react-a11y-demo/src/HomePage.js
@@ -22,8 +22,23 @@ class HomePage extends Component {
           }}
          >
          Welcome to my A11y React Demo</h2>
-        <p>I will be presenting examples of accessible React components here. This is a work in progress. The code can be found at <a href="https://github.com/clane/React-A11y-Repo">https://github.com/clane/React-A11y-Repo</a>.</p>
+        <p>These examples demonstrate the following accessibility concepts:</p>
+        <ul>
+          <li>Contextual page titles</li>
+          <li>Skip link</li>
+          <li>Focus management when routing</li>
+          <li>Focus management between components</li>
+          <li>General keyboard accessibility</li>
+          <li>Use of a polite live region</li>
+          <li>Use of Offscreen text</li>
+          <li>ARIA grid roles</li>
+          <li>Dual focus using aria-activedescendant</li>
+          <li>Use of aria-label</li>
+          <li>Use of aria-hidden</li>
+        </ul>
+        <p>The code can be found at <a href="https://github.com/clane/React-A11y-Repo">https://github.com/clane/React-A11y-Repo</a>.</p>
       </div>
+         
     );
   }
 }
