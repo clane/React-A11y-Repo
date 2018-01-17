807092a499cff5ce1be8a84aae18ceab9e7228ac
Chris Lane <chris.lane359@gmail.com>
2018-01-17 06:03:22 -0800

    array of refs now working

--- react-a11y-demo/src/RefsTest.js
+++ react-a11y-demo/src/RefsTest.js
@@ -11,22 +11,28 @@ class RefsTest extends Component {
 
 componentDidMount(){
 console.log('mounted');
-console.log(this.nodes[0]);
-this.nodes[0].focus();
+this.nodes[4].focus();
 
 } 
 
 render() {
   //http://www.mattzabriskie.com/blog/react-referencing-dynamic-childrenhttp://www.mattzabriskie.com/blog/react-referencing-dynamic-children
   
-   var index = 0; 
+   //var index = 0; 
 
     return (
-      <div tabIndex="-1" ref={node => {this.nodes[index] = node;}} >
-        foo
-        {index}
+      <div>
+      <div tabIndex="-1" ref={node => {this.nodes[0] = node;}} >0</div>
+      <div tabIndex="-1" ref={node => {this.nodes[1] = node;}} >1</div>
+      <div tabIndex="-1" ref={node => {this.nodes[2] = node;}} >2</div>
+      <div tabIndex="-1" ref={node => {this.nodes[3] = node;}} >3</div>
+      <div tabIndex="-1" ref={node => {this.nodes[4] = node;}} >4</div>
+      <div tabIndex="-1" ref={node => {this.nodes[5] = node;}} >5</div>
+      <div tabIndex="-1" ref={node => {this.nodes[6] = node;}} >6</div>
+      <div tabIndex="-1" ref={node => {this.nodes[7] = node;}} >7</div>
+      <div tabIndex="-1" ref={node => {this.nodes[8] = node;}} >8</div>
+      <div tabIndex="-1" ref={node => {this.nodes[9] = node;}} >9</div>
       </div>
-
     );
   }
 
