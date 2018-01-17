72bbeeb789d189da96fe31085ce3bf7672ac41f2
Chris Lane <chris.lane359@gmail.com>
2018-01-17 07:43:14 -0800

    dynamic references now working

--- react-a11y-demo/src/RefsTest.js
+++ react-a11y-demo/src/RefsTest.js
@@ -7,11 +7,13 @@ class RefsTest extends Component {
     super(props);
     console.log(props);
     this.nodes = [];
+    this.linkRefs = [];
   }
 
 componentDidMount(){
 console.log('mounted');
-this.nodes[4].focus();
+//this.nodes[4].focus();
+this.linkRefs[4].focus();
 
 } 
 
@@ -19,19 +21,30 @@ render() {
   //http://www.mattzabriskie.com/blog/react-referencing-dynamic-childrenhttp://www.mattzabriskie.com/blog/react-referencing-dynamic-children
   
    //var index = 0; 
+let links = [];
+    for (let i = 0; i < 50; i++) {
+      let href = i + ".html";
+      let linkText = "link " + i;
+      links.push(<a ref={link => {this.linkRefs[i] = link;}} href={href} key={i}>{linkText}</a>);
+    }
 
     return (
       <div>
-      <div tabIndex="-1" ref={node => {this.nodes[0] = node;}} >0</div>
-      <div tabIndex="-1" ref={node => {this.nodes[1] = node;}} >1</div>
-      <div tabIndex="-1" ref={node => {this.nodes[2] = node;}} >2</div>
-      <div tabIndex="-1" ref={node => {this.nodes[3] = node;}} >3</div>
-      <div tabIndex="-1" ref={node => {this.nodes[4] = node;}} >4</div>
-      <div tabIndex="-1" ref={node => {this.nodes[5] = node;}} >5</div>
-      <div tabIndex="-1" ref={node => {this.nodes[6] = node;}} >6</div>
-      <div tabIndex="-1" ref={node => {this.nodes[7] = node;}} >7</div>
-      <div tabIndex="-1" ref={node => {this.nodes[8] = node;}} >8</div>
-      <div tabIndex="-1" ref={node => {this.nodes[9] = node;}} >9</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[0] = node;}} >0</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[1] = node;}} >1</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[2] = node;}} >2</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[3] = node;}} >3</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[4] = node;}} >4</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[5] = node;}} >5</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[6] = node;}} >6</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[7] = node;}} >7</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[8] = node;}} >8</div>
+	      <div tabIndex="-1" ref={node => {this.nodes[9] = node;}} >9</div>
+
+
+              <div role="navigation" aria-label="navigation menu">
+                {links}        
+              </div>
       </div>
     );
   }
