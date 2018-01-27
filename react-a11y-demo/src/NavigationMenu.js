340d91fe171fa907c92d51e9565002436fbc972d
Chris Lane <chris.lane359@gmail.com>
2018-01-27 14:18:13 -0800

    Revert "Revert "Revert "Revert "saving""""
    This reverts commit cc5f78249b31ccab379f584bd2f000fcdae98042.

--- react-a11y-demo/src/NavigationMenu.js
+++ react-a11y-demo/src/NavigationMenu.js
@@ -169,9 +169,8 @@ class NavigationMenu extends Component {
   }
   
 	choiceHandler(e, index){
-    if (e.keyCode === 13) {
-      alert('choice handled'); 
-		}
+		console.log('in choiceHandler index = ' + index);
+    alert('choice handled'); 
 	}
 
   componentDidMount() {
@@ -251,6 +250,7 @@ class NavigationMenu extends Component {
                       role="menuitem"
                       data-active={choice.choicesActiveDescendant}
                       onKeyDown={e => this.choiceHandler(e, index)}
+                      onClick={e => this.choiceHandler(e, index)}
                     >
                       {choice.name}
                     </div>
